e423cb7ded094dbed028e1af3531c0b617c954fb
jxcjxcjzx <jxcjxcjzx@sina.com>
2016-07-31 02:52:02 +0000

    normal update 0731

--- db/mongo/insert.js
+++ db/mongo/insert.js
@@ -1,9 +1,15 @@
-ertDocuments = function(db, callback) {
+
+var  assert = require('assert');
+
+
+insertDocuments = function(db,content, callback) {
   // Get the documents collection 
   var collection = db.collection('documents');
   // Insert some documents 
+  // describ, title, detail
+
   collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
+    {describ : content.describ}, {title : content.title}, {detail : content.detail}
   ], function(err, result) {
     assert.equal(err, null);
     assert.equal(3, result.result.n);
@@ -11,4 +17,12 @@ ertDocuments = function(db, callback) {
     console.log("Inserted 3 documents into the document collection");
     callback(result);
   });
+}
+
+
+
+module.exports = {
+
+	insert : insertDocuments
+
 }
--- python/db.py
+++ python/db.py
@@ -16,21 +16,32 @@ sys.setdefaultencoding( "utf-8" )
 def analyze_single_page(pageUrl):
 	html = requests.get(pageUrl,timeout=20,verify=False).text
 	soup = BeautifulSoup(html)
-	newurl = ''
-	newcontent = ''
 	hostname = "https://www.npmjs.com"
+	details = ''
 	li_url_list = []
 	ul_level_list = soup.find_all('ul', class_='search-results') 
 	[li_url_list.extend(item.find_all('li')) for item in ul_level_list]
 	total_describ_set =set()
+	total_detail_set =set()
 	for li_str in li_url_list:
 		if not li_str.find('a'):
 			continue
-		newurl =  hostname+li_str.find_all("a",{"class":"name"})[0].attrs['href'] 
-		newcontent = requests.get(newurl,timeout=20,verify=False).text
-		total_describ_set.add( li_str.find_all("p",{"class":"description"})[0].text   + " === " + li_str.find_all("a",{"class":"name"})[0].text  + " === " + newcontent )
+		details = analysis_single_page_detail(hostname+li_str.find_all("a",{"class":"name"})[0].attrs['href']) 
+		total_describ_set.add( li_str.find_all("p",{"class":"description"})[0].text   + " === " + li_str.find_all("a",{"class":"name"})[0].text  + " === " + details )
+
 	codecs.open('total_project_list.txt', mode='ab', encoding='utf-8').writelines([ item+'\n' for item in total_describ_set])
 	
+def analysis_single_page_detail(pageUrl):
+	html = requests.get(pageUrl,timeout=20,verify=False).text
+	soup = BeautifulSoup(html)
+	detail = ''
+	div_readme_list = soup.find_all('div',{"id":"readme","class":"markdown"})
+	div_readme = div_readme_list[0]
+	for item in div_readme.find_all("p"):
+		detail += item.text
+	return detail
+	
+
 
 if __name__ == "__main__":
 	analyze_single_page("http://www.npmjs.com/search?q=z")
