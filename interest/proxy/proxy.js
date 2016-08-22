38a5384a84b40fe51af5a6908ed27e0a2363179b
jxcjxcjzx <jxcjxcjzx@sina.com>
2016-08-22 14:56:46 +0000

    update new way to proxy requests, now a new android side browser is needed. : ) nice day

--- interest/proxy/proxy.js
+++ interest/proxy/proxy.js
@@ -1,33 +1,37 @@
-var proxy = require("http-proxy").createProxyServer({});
 
-var http = require("http");
+var http = require("http");                                                                                                          
+                                                                                                                                     
+var fs = require("fs");                                                                                                              
+                                                                                                                                     
+var server = http.createServer(function(request,response){                                                                           
+                                                                                                                                     
+                                                                                                                                     
+        console.log("got one request");                                                                                              
+                                                                                                                                     
+        var path = request.url.slice(1);    
+        
+        http.get(path,function(res){                                                                                                 
+                                                                                                                                     
+                var html = '';                                                                                                       
+                // now trick is needed here                                                                                          
+                res.pipe(response);                                                                                                  
+                                                                                                                                     
+                res.on('data',function(data){                                                                                        
+                        html += data;                                                                                                
+                })                                                                                                                   
+                                                                                                                                     
+                res.on("end",function(){                                                                                             
+                        console.log("end .. ");                                                                                      
+                        filename = path | "empty";                                                                                   
+                        fs.writeFile('index.html', html, function(err) {                                                             
+                                if (err) {                                                                                           
+                                }                                                                                                    
+                                        console.log('content stored ... ');                                                          
+                        });                                                                                                          
+                });                                                                                                                  
+        });                                                                                                                          
+});                                                                                                                                  
+                                                                                                                                     
+                                                                                                                                     
+server.listen(81);                                                                                                                   
 
-
-var server = http.createServer(function(request,response){
-	
-
-	console.log("got one request");
-
-	var path = request.url;
-
-
-//	console.log("request path is : "+request.url);
-
-		
-	http.get(path,function(res){
-
-		// now trick is needed here
-		res.pipe(response);
-
-	}).end();
-
-			
-	
-	
-});
-
-
-server.listen(80);
-
-
-// how to use this :  ---  curl -x http://localhost:80  www.sina.com.cn   
