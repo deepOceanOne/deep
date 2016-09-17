bece34813aeeb8e8d33507d4bd5d36d0a7ca7964
jxcjxcjzx <jxcjxcjzx@sina.com>
2016-09-17 04:57:54 +0000

    normal update, add crontab and task file, useful now

---
+++ db/cron/wget_task_1.sh
@@ -0,0 +1,5 @@
+basedir='/root/develop/deep/db/web_diff/'
+wget www.163.com -O $basedir"www_163_com_"` date "+%Y_%m_%d_%H_%M_%S" `.html
+wget www.sina.com.cn -O $basedir"www_sina_com_cn_"` date "+%Y_%m_%d_%H_%M_%S" `.html
+wget www.jianshu.com -O $basedir"www_jianshu_com_"` date "+%Y_%m_%d_%H_%M_%S" `.html
+
